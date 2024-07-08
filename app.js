259491f87416c729e8864d49ce156cf596c76ab1
Sami Uusitalo <sami.uusitalo@op.fi>
2024-07-08 14:08:49 +0300

    minor updates

--- app.js
+++ app.js
@@ -14,8 +14,8 @@ logger.info('port', config.PORT);
 
 mongoose
   .connect(config.MONGODB_URI)
-  .then(() => {
-    logger.info('connected to MongoDB');
+  .then((result) => {
+    logger.info('connected to MongoDB', result.models);
   })
   .catch((error) => {
     logger.error('error connection to MongoDB:', error.message);
